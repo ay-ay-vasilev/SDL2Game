3e99a14d4f680c5a20c70031b54e01e3ff149623
ay-ay-vasilev <ay.ay.vasilev@gmail.com>
2023-02-07 23:44:39 +0700

    FPS lock

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,29 @@
 #endif
 #include "Game.h"
 #include <memory>
+#include <chrono>
+#include <thread>
 
 std::unique_ptr<Game> game;
 
 int main(int argc, char* argv[])
 {
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+
 	game = std::make_unique<Game>();
 	game->init("Game Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
+	auto nextFrame = std::chrono::steady_clock::now();
 	while (game->running())
 	{
 		game->handleEvents();
 		game->update();
 		game->render();
-	}
 
+		nextFrame += std::chrono::milliseconds(frameDelay);
+		std::this_thread::sleep_until(nextFrame);
+	}
 	game->clean();
 
 	return 0;
